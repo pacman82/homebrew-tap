62b66ffb6833f94aaa8e3bb5fc8410e88f452f00
Markus Klein <markus-klein@live.de>
2020-12-10 21:54:27 +0100

    fix copy & paste error

--- Formula/odbc2parquet.rb
+++ Formula/odbc2parquet.rb
@@ -1,8 +1,7 @@
-class DiCsv2xml < Formula
+class Odbc2parquet < Formula
     desc "Query an ODBC data source and store the result in a Parquet file."
     homepage "https://github.com/pacman82/odbc2parquet"
     url "https://github.com/pacman82/odbc2parquet/archive/v0.5.1.tar.gz"
-    sha256 "2789d48959e8742498c87e1b19c71aac0ffa1f12a40e479f329e4a1140a02169"
     sha256 "d0df1f6ce71c926a2e72053c8bf25df1e8f7ccf2b802530ac17d58b3ba3c9f51"
   
     depends_on "rust" => :build
